9b75bed21b54dc4a602c9d20c565990a733fa73d
StarryPurp <starbloom128@gmail.com>
2017-06-20 20:56:03 +0900

    #6 Thread seekbar

--- app/src/main/java/bstar128/example/hansung/musicplaylists/MainActivity.java
+++ app/src/main/java/bstar128/example/hansung/musicplaylists/MainActivity.java
@@ -1,6 +1,7 @@
 package bstar128.example.hansung.musicplaylists;
 
 import android.media.MediaPlayer;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,18 +10,22 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.SeekBar;
 import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
 
 public class MainActivity extends AppCompatActivity {
     ListView listV;
     Button butPlay,butStop,butPause;
-    TextView textMusic;
-    ProgressBar progress;
+    TextView textMusic,textTime;
+    SeekBar progress;
     String[] musicArray={"movie","sometime","secret"};
     int[] musiResid={R.raw.movie,R.raw.sometime,R.raw.secret};
     int selectedMusicid;
     MediaPlayer Mplayer;
     boolean selectPause;
+    int i;
 
 
     @Override
@@ -32,7 +37,8 @@ public class MainActivity extends AppCompatActivity {
         butStop=(Button)findViewById(R.id.but_stop);
         butPause=(Button)findViewById(R.id.but_pause);
         textMusic=(TextView)findViewById(R.id.text_music);
-        progress=(ProgressBar)findViewById(R.id.prograss_ing);
+        textTime=(TextView)findViewById(R.id.text_time);
+        progress=(SeekBar) findViewById(R.id.prograss_ing);
         ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_single_choice,musicArray);
         listV.setAdapter(adapter);
         listV.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
@@ -46,19 +52,38 @@ public class MainActivity extends AppCompatActivity {
                 selectPause=false;
                 Mplayer.stop();
                 selectedMusicid=musiResid[i];
-                progress.setVisibility(View.INVISIBLE);
+                MainActivity.this.i=i;
             }
         });
         butPlay.setOnClickListener(new View.OnClickListener() {
+            SimpleDateFormat timeFormat=new SimpleDateFormat("mm:ss");
             @Override
             public void onClick(View v) {
+                textMusic.setText(musicArray[i]);
                 if(selectPause) {
                     selectPause=false;//시작하고 ㄱㄱ~
                 }
                 else
                     Mplayer=MediaPlayer.create(MainActivity.this,selectedMusicid);
                 Mplayer.start();
-                progress.setVisibility(View.VISIBLE);
+                Thread musicthread=new Thread(){
+                    @Override
+                    public void run(){
+                        if(Mplayer==null) return;
+                        progress.setMax(Mplayer.getDuration());//전체 음악 재생길이
+                        while(Mplayer.isPlaying()){
+                            progress.setProgress(Mplayer.getCurrentPosition());
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    textTime.setText("진행시간: "+timeFormat.format(Mplayer.getCurrentPosition()));//시간보여주기
+                                }
+                            });
+                            SystemClock.sleep(200);
+                        }
+                    }
+                };
+                musicthread.start();
             }
         });
         butPause.setOnClickListener(new View.OnClickListener() {
@@ -66,7 +91,6 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 selectPause=true;
                 Mplayer.pause();
-                progress.setVisibility(View.INVISIBLE);
             }
         });
         butStop.setOnClickListener(new View.OnClickListener() {
@@ -74,7 +98,6 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 selectPause=false;
                 Mplayer.stop();
-                progress.setVisibility(View.INVISIBLE);
             }
         });
     }
